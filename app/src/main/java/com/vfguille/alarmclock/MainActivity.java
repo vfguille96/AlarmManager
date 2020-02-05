006b92063b5dd670c42b95ac4be25778c0eb20fb
Guillermo Vera <vfguille@gmail.com>
2020-02-05 09:11:04 +0100

    Alarm finish

--- app/src/main/java/com/vfguille/alarmclock/MainActivity.java
+++ app/src/main/java/com/vfguille/alarmclock/MainActivity.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.app.TimePickerDialog;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     Button button;
     TextView textView;
     Calendar calendar;
+    BroadcastReceiver broadcastReceiver;
+    private final static String TAG = "BR";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,12 +42,28 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (calendar != null){
                     setAlarmManager();
-                    finish();
+                    //finish();
+                    Intent intent = new Intent("com.vfguille.alarmclock");
+                    sendBroadcast(intent);
                 }
             }
         });
+
+        broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                IntentFilter intentFilter = new IntentFilter();
+                intentFilter.addAction("com.vfguille.alarmclock");
+                registerReceiver(broadcastReceiver, intentFilter);
+            }
+        };
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(broadcastReceiver);
+    }
 
     /**
      * Establece una alarma en el sistema
@@ -53,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent("com.vfguille.alarmclock");
         PendingIntent pendingIntent = PendingIntent.getBroadcast(this, REQUEST_CODE, intent, PendingIntent.FLAG_UPDATE_CURRENT);
         AlarmManager alarmManager = (AlarmManager) this.getSystemService(ALARM_SERVICE);
-        alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
     }
 
     private void createTimePickerDialog() {
